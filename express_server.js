d12224e6c371f323966028edce7939c5365b6220
Charlie C <zbyabqrc@pm.me>
2023-05-24 04:58:56 +0000

    Created new users global object to store and access users in the app

--- express_server.js
+++ express_server.js
@@ -19,6 +19,18 @@ function generateRandomString() {
   
   return result;
 }
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
